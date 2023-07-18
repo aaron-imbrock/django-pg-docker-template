6f6dde6698d190d78833003e3af2817f2bcbdba3
Aaron Imbrock <aaron@sizza.net>
2023-07-18 17:54:37 -0600

    Test of ruff. Run on one file.

--- books/models.py
+++ books/models.py
@@ -5,25 +5,22 @@ from django.urls import reverse
 
 
 class Book(models.Model):
-    id = models.UUIDField(
-        primary_key=True,
-        default=uuid.uuid4,
-        editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     cover = models.ImageField(upload_to="covers/", blank=True)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=["id"], name="id_index"),
+        ]
+        
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[str(self.id)])
-
-    class Meta:
-        indexes = [
-            models.Index(fields=["id"], name="id_index"),
-        ]
 
 
 class Review(models.Model):
